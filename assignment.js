780ec03aa418f735e13efb592d2128e0da69c6f2
nasimuk47 <nasim.uk47@gmail.com>
2023-08-10 01:00:04 +0600

    more adding

--- assignment.js
+++ assignment.js
@@ -1,37 +1,38 @@
 // problem1------------------------
-// function cubeNumber(number) {
-//     if (typeof number === "number") {
-//         result = number ** 3;
-//         return result;
-//     } else {
-//         if (typeof number !== "number") {
-//             return "please provide a number";
-//         }
-//     }
-// }
+function cubeNumber(number) {
+    if (typeof number === "number") {
+        result = number ** 3;
+        return result;
+    } else {
+        if (typeof number !== "number") {
+            return "please provide a number";
+        }
+    }
+}
 
-// console.log(cubeNumber(3));
+const cubeResult = cubeNumber(4);
 
-// console.log(cubeNumber("jamud"));
+console.log(cubeResult);
 
 // problem2------------------------------
 
-// function matchFinder(String1, String2) {
-//     if (typeof String1 !== "string" || typeof String2 !== "string") {
-//         return "please provide me a strings ";
-//     }
+function matchFinder(String1, String2) {
+    if (typeof String1 !== "string" || typeof String2 !== "string") {
+        return "please provide me a strings ";
+    }
 
-//     String1 = String1.toLowerCase();
-//     String2 = String2.toLowerCase();
+    String1 = String1.toLowerCase();
+    String2 = String2.toLowerCase();
 
-//     if (String1.includes(String2)) {
-//         return true;
-//     } else {
-//         return false;
-//     }
-// }
+    if (String1.includes(String2)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+const stringname = matchFinder("Peter Parker", "Pen");
 
-// console.log(matchFinder("Bangladesh", "Desh"));
+console.log(stringname);
 
 // problem3--------------------------------------
 
@@ -52,6 +53,47 @@ function sortMaker(arr) {
 
     return arr.sort((a, b) => b - a);
 }
-//
-// Sample inputs
-console.log(sortMaker([4, -2]));
+
+const dataList = sortMaker([4, -2]);
+
+console.log(dataList);
+
+// problem4------------------
+
+function findAddress(obj) {
+    const properties = ["street", "house", "society"];
+    let output = [];
+
+    for (let i = 0; i < properties.length; i++) {
+        const property = properties[i];
+
+        if (obj.hasOwnProperty(property)) {
+            output.push(obj[property]);
+        } else {
+            output.push("__");
+        }
+    }
+
+    return output.join(",");
+}
+
+const address = findAddress({ street: 10 });
+console.log(address);
+
+// problem5----------------------------------
+
+function canPay(arry, number) {
+    if (arry.length === 0) {
+        return "The array is empty";
+    }
+
+    let total = 0;
+    for (let i = 0; i < arry.length; i++) {
+        total += arry[i];
+    }
+
+    return total >= number;
+}
+
+const budget = canPay([1, 5, 5], 10);
+console.log(budget);
